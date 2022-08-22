e4c87d9d0c59b7a3b241cd72f643a3df20e74254
Manikanta H <67575982+mani733@users.noreply.github.com>
2022-08-22 21:08:11 +0530

    inserted one data

--- Microservice/JAVA/Associates - Manager/home-manager/src/main/resources/data.sql
+++ Microservice/JAVA/Associates - Manager/home-manager/src/main/resources/data.sql
@@ -4,6 +4,6 @@ values(101,'Karthik','H','2021-02-09T11:19:42.12Z');
 insert into home_manager(manager_id,first_name,last_name,reg_date)
 values(102,'Jobin','J','2021-02-09T11:19:42.12Z');
 
-
-
+insert into home_manager(manager_id,first_name,last_name,reg_date)
+values(103,'Manikanta','H','2021-02-09T11:19:42.12Z');
 
